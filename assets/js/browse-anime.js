7091dd5a6cc415792ab9fd299d680279ee425181
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-07-09 13:26:52 -0400

    made browse page responsive by changing html and css

--- assets/js/browse-anime.js
+++ assets/js/browse-anime.js
@@ -6,6 +6,7 @@ console.log(browseAnimeCards.children()[0]);
 var browseAnime = function () {
     fetch(
         // fetching data from kitsu based on anime popularity, max 12 items per page, first page
+        // maybe sort by alphabetical order? My hero academia seasons 1-3 show up as different anime
         'https://kitsu.io/api/edge/anime?sort=popularityRank&page[limit]=12&page[offset]=0'
     )
         .then(function (response) {
@@ -16,7 +17,7 @@ var browseAnime = function () {
             // looping through all 12 results
             for (i = 0; i < 12; i++) {
                 // creating the title element and putting text in it
-                var browseAnimeTitle = $('<h2></h2>');
+                var browseAnimeTitle = $('<h2 class="title is-6"></h2>');
                 var englishTitle = data.data[i].attributes.titles.en;
                 var japaneseTitle = data.data[i].attributes.titles.en_jp;
                 if (englishTitle == undefined) {
@@ -26,7 +27,7 @@ var browseAnime = function () {
                 }
                 console.log(browseAnimeTitle);
                 // creating the image element and putting a src in it
-                var browseAnimeImage = $('<img></img>');
+                var browseAnimeImage = $('<img class="anime-browse-img is-12-mobile is-3-desktop"></img>');
                 browseAnimeImage.attr('src', data.data[i].attributes.coverImage.original);
                 console.log(browseAnimeImage);
                 // grabbing the anime id from the api and adding it to the existing anchor
