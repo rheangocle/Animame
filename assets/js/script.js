66db47325247c54cc5e5d42d644fdb6508e0c899
Rhea Le <106790436+rheangocle@users.noreply.github.com>
2022-07-10 23:07:16 -0400

    added js code to generate specific food cards based on anime english title

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,9 +7,7 @@ var formSubmitHandler = function (event) {
   event.preventDefault();
   var animeTitle = searchInput.value.trim();
   //need to add anime search to function in anime js for fetch url
-  searchAnime(animeTitle);
   document.location.href = "./anime.html";
 }
-
 
 searchBtn.addEventListener('click', formSubmitHandler);
